514c072d2869b879cc2de8e7db7db1bb7a1981cd
icy-veins <simple-developer@hotmail.com>
2022-06-22 16:01:44 +0100

    added gitignore

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { get } from 'http';
 const app = express();
+import cors from 'cors';
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.get('/home', (req, res) => {
     res.json({success: true, payload: "Hello world"});
 });
-
+console.log()
 app.listen(port, () => console.log("Listening"));
